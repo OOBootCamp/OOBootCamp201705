81b68ee3c5ef3418405cb15c46c705a7c0ac337c
jianwang <jianwang@thoughtworks.com>
2017-05-20 17:31:26 +0800

    refactor.

--- src/main/java/PickHelper.java
+++ src/main/java/PickHelper.java
@@ -1,7 +1,7 @@
 import java.util.List;
 
 public class PickHelper {
-    <T extends  ParkingAble> Car pick(Token token, List<T> parkingLots) {
+    Car pick(Token token, List<? extends ParkingAble> parkingLots) {
         for (ParkingAble parkingLot : parkingLots)
             if (parkingLot.containToken(token)) return parkingLot.pick(token);
         throw new CarNotExistException();
